fd04d98a6834605cdb4cb15ed35ec93758d67a65
AnitaGj1 <anitaristovska@live.com>
2023-05-23 10:53:54 -0400

    adding starter code and resorces

---
+++ static/js/app.js
@@ -0,0 +1,52 @@
+// 1. Read in the JSON data
+
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+
+// Fetch the JSON data
+
+  // Extract the relevant arrays from the data object
+  
+
+  // Create the trace for the horizontal bar chart
+  
+
+  // Create the data array
+  
+
+  // Define the layout
+
+
+  // Plot the chart
+ 
+
+
+// 3. Create a bubble chart that displays each sample.
+
+
+// Fetch the JSON data
+
+
+  // Create the trace for the bubble chart
+ 
+
+  // Create the data array
+
+
+  // Define the layout
+  
+
+  // Plot the chart
+
+
+
+// 4. Display the sample metadata, i.e., an individual's demographic information.
+
+
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+
+// 6. Update all the plots when a new sample is selected.
