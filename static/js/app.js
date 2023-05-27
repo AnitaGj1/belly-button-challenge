7fc227366c699956101c10be9822d9018f89f6b5
AnitaGj1 <anitaristovska@live.com>
2023-05-27 18:12:40 -0400

    adding final code and images

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,127 @@
-// 1. Read in the JSON data
-
+// Place url in a constant variable
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// 1. Read in the JSON data &
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+d3.json(url).then(function(data) {
+    console.log(data);
 
+    // 2. Dropdown menu
 
-// Fetch the JSON data
+    const dropDown = d3.select("#selDataset");
 
-  // Extract the relevant arrays from the data object
+    const names = data.names;
+    
+    for (let i = 0; i < names.length; i++) {
+      let option = dropDown.append("option");
+      option.property("value", names[i]);
+      option.text(names[i]);
+    }
   
+    // Initial charts with the first sample
+    barChart(names[0]);
+    bubbleChart(names[0]);
+    demographicInfo(names[0])
+  });
 
-  // Create the trace for the horizontal bar chart
+  // 3. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+function barChart(sample) {
+    d3.json(url).then(function(data) {
+      // Filter the data for the selected sample
+      const sampleData = data.samples.filter(obj => obj.id === sample)[0];
   
-
-  // Create the data array
+      // Get the top 10 OTUs and reverse the order for horizontal bar chart
+      const top10OTUs = sampleData.otu_ids.slice(0, 10).reverse();
+      const top10Values = sampleData.sample_values.slice(0, 10).reverse();
+      const top10Labels = sampleData.otu_labels.slice(0, 10).reverse();
   
-
-  // Define the layout
-
-
-  // Plot the chart
- 
-
-
-// 3. Create a bubble chart that displays each sample.
-
-
-// Fetch the JSON data
-
-
-  // Create the trace for the bubble chart
- 
-
-  // Create the data array
-
-
-  // Define the layout
+      // Create the trace for the bar chart
+      const barTrace = {
+        x: top10Values,
+        y: top10OTUs.map(otu => `OTU ${otu}`),
+        text: top10Labels,
+        type: "bar",
+        orientation: "h"
+      };
   
+      // Create the data array for the plot
+      const barData = [barTrace];
+  
+      // Define the layout for the bar chart
+      const barLayout = {
+        title: `Top 10 OTUs for Sample ${sample}`,
+        xaxis: { title: "Sample Values" },
+        yaxis: { title: "OTU IDs" }
+      };
+  
+      // Plot the bar chart
+      Plotly.newPlot("bar", barData, barLayout);
+    })
+};
 
-  // Plot the chart
+ // 4. Create a bubble chart that displays each sample.
+    function bubbleChart(sample) {
+        d3.json(url).then(function(data) {
+          // Filter the data for the selected sample
+          const sampleData = data.samples.filter(obj => obj.id === sample)[0];
+      
+          // Get the top 10 OTUs and reverse the order for horizontal bar chart
+          const OTUs = sampleData.otu_ids;
+          const values = sampleData.sample_values;
+          const labels = sampleData.otu_labels;
+     // Create the trace for the bubble chart
+
+     const bubbleTrace = {
+        x: OTUs,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+          size: sampleData.sample_values,
+          color: sampleData.otu_ids,
+          colorscale: "Earth"
+        }
+        
+    };
+  
+      // Create the data array for the bubble chart
+      const bubbleData = [bubbleTrace];
+  
+      // Define the layout for the bubble chart
+      const bubbleLayout = {
+        title: `OTU IDs vs. Sample Values for Sample ${sample}`,
+        xaxis: { title: "OTU IDs" },
+        yaxis: { title: "Sample Values" },
+        showlegend: false
+      };
+  
+      // Plot the bubble chart
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    })
+};
+  
+  
+// 5. Display the sample metadata, i.e., an individual's demographic information.
+
+function demographicInfo(sample) {
+    d3.json(url).then(function(data) {
+      const metadata = data.metadata;
+      const result = metadata.filter(obj => obj.id == sample)[0];
+      const panel = d3.select("#sample-metadata");
+  
+      panel.html("");
+  
+      Object.entries(result).forEach(([key, value]) => {
+        panel.append("p").text(`${key}: ${value}`);
+      });
+    });
+};
 
 
-
-// 4. Display the sample metadata, i.e., an individual's demographic information.
-
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-
-// 6. Update all the plots when a new sample is selected.
+// 6. Update all the plots when a new sample is selected. 
+function optionChanged(value) {
+    console.log(value);
+    barChart(value);
+    bubbleChart(value)
+    demographicInfo(value);
+  };
